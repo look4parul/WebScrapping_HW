bc53f681b19ad817430d016e10d7f5b2c18d35fd
Parul <parul.g27@gmail.com>
2020-03-02 12:38:54 -0600

    Add geckodriver and format the code

--- app.py
+++ app.py
@@ -1,34 +1,31 @@
+# Dependencies
 from flask import Flask, render_template, redirect
 # Module used to connect Python with MongoDb
 import pymongo
 from flask_pymongo import PyMongo
+# Import the python file used for web scrapping
 import scrape_mars
 
 app = Flask(__name__)
 
 # Default port used by mongodb is 27017
-# conn = "mongodb://localhost:27017"
-# client = pymongo.MongoClient(conn)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/marsDB"
 mongo = PyMongo(app)
-# Define the mardDB database
-#db = client.marsDB
-
+# Create a route that will query Mongo database and sends initial dict values of mars database into an HTML template to display the data.
 @app.route("/")
 def home():
     mars_data = mongo.db.marsDB.find_one()
     return render_template("index.html", mars_data=mars_data)
 
+# Create a route that displays all of the information that was scraped in scrape.py file
 @app.route("/scrape")
 def scraper():
 # Collection
     mars = mongo.db.marsDB
+    # Write the scraped data in database
     mars_data = scrape_mars.scrape()
     mars.update({}, mars_data, upsert = True)
     return redirect("/", code=302)
-
-# for data in db.mars.find():
-#     print(type(data))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,54 +1,65 @@
+# Dependencies
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
 from splinter import browser
 from selenium import webdriver
-import urllib.request
 
-# Create a dict for mars data collected using web scrapping
-#mars_facts_data = {}
-
-## NASA MARS NEWS
+# Define the scrape function for web scrapping from the all the URL's 
 def scrape():
     # Create a dict for mars data collected using web scrapping
     mars_results = {}
+    ######################################################################################
+    ## NASA MARS NEWS
+    ######################################################################################
+    
+    # News URL
     url = "https://mars.nasa.gov/news"
+    # Use selenium webdriver to retrieve the page information
     driver = webdriver.Firefox()
     driver.get(url)
     html = driver.page_source
     # Scrape the webpage using beautiful soup
     soup = bs(html, 'html.parser')
+    driver. quit() 
     # Latest title and remove the new line 
     news_title = soup.find('div', class_ = 'content_title').get_text(strip=True)
-    # print("News Title: ", news_title)
     # Latest Paragraph
     news_p = soup.find('div', class_ = 'article_teaser_body').get_text()
-    #latest_news={"title":news_title,"text":news_p}
     mars_results["News_title"] = news_title
     mars_results["News_p"] = news_p
+
     ######################################################################################
-    # #### Featured Image
+    #### Featured Image
     ######################################################################################
+
+    # Featured Image URL
     image_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     # Retrieve page with the requests module
     response = requests.get(image_url)
+    # Scrape the webpage using beautiful soup
     soup = bs(response.text, 'html.parser')
+    # Featured URL
     featured_url = soup.find("article", class_="carousel_item")["style"]
     featured_url = featured_url.strip("background-image: url('")
     featured_url = featured_url.strip("');")
-    print("featured_url: "+featured_url)
     featured_image_url = "https://www.jpl.nasa.gov" + featured_url
     mars_results["featured_image_url"] = featured_image_url
     
     ######################################################################################
     #### Latest_weather
     ######################################################################################
+
+    # Wether URL
     weather_url = "https://twitter.com/marswxreport?lang=en"
+    # Use selenium webdriver to retrieve the page information
     response = requests.get(weather_url)
     driver = webdriver.Firefox()
     driver.get(weather_url)
     html = driver.page_source
+    # Scrape the webpage using beautiful soup
     soup = bs(response.text, 'html.parser')
+    driver. quit() 
     # Extract the tweet text
     mars_weather = soup.find(class_='tweet-text').text
     mars_results["mars_weather"] = mars_weather
@@ -56,33 +67,41 @@ def scrape():
     ######################################################################################
     #### Mars Facts
     ######################################################################################
+    
+    # Mars Facts URL
     facts_url = "http://space-facts.com/mars/"
+    
+    # Convert the data to html
     data = pd.read_html(facts_url)
+    # Convet the html to the pandas DataFrame
     df = data[0]
-    #df.columns = ["Mars-Earth Comparison", "Mars", "Earth"]
     df.columns = ["Fact","Value"]
     # Get rid of trailing colon
     df["Fact"] = df["Fact"].str[:-1]
     df = df.set_index("Fact")
-    print(df)
-
+    # Convert he data to html to display the scraped info as an html table
     facts_html_table = df.to_html()
     facts_html_table = facts_html_table.replace('\n', '')
-    #mars_facts = df.to_html()
-    #facts_dict = dict(mars_facts)
-    #print(mars_facts)
     mars_results['facts_html_table'] = facts_html_table
     
     ######################################################################################
     #### Hemisphere_info
     ######################################################################################
+
+    # Mars Hemisphere URL 
     hemisphere_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    # Retrieve page with the requests module
     response = requests.get(hemisphere_url)
+    # Scrape the webpage using beautiful soup
     soup = bs(response.text, 'html.parser')
+    
+    # Create an empty list to get title and url details
     hemi_img_url_list = []
     title = []
     for item in soup.select("a.itemLink"):
+        # title
         title.append(item.select("h3")[0].get_text())
+        # URL for the image
         url_new = "https://astrogeology.usgs.gov"+item["href"]
         response_new = requests.get(url_new)
         soup_new = bs(response_new.text, 'html.parser')
@@ -91,68 +110,4 @@ def scrape():
         
     mars_results["hemisphere_image_urls"] = hemisphere_image_urls
     return mars_results
-    #hemisphere_image_urls
-
-# #Now we put all the data into Mongo:
-
-#             # Initialize PyMongo to work with MongoDBs
-#             # conn = 'mongodb://localhost:27017'
-#             # conn="mongodb://heroku_dxww20g1:2m85ei2jvb8o3u8j6r994d8rqh@ds263791.mlab.com:63791/heroku_dxww20g1"
-            
-
-
-#     str0=os.environ.get("MONGODB_URI")
-#             # str0="mongodb://heroku_dxww20g1:2m85ei2jvb8o3u8j6r994d8rqh@ds263791.mlab.com:63791/heroku_dxww20g1"
-#     print("str0=",str0)
-#     str1=str0.split('/');str1.pop;Mars_db=str1[-1];
-#     print("str1=",str1)
-#     str0='/'.join(str1)
-           
-
-#     conn=str0
-#     client = pymongo.MongoClient(conn)
-
-            
-#             # Define database and collection
-#             # client.drop_database('Mars_db')
-#             # db = client.Mars_db
-
-#     client.drop_database(Mars_db)
-#     db = client[Mars_db]
-
-
-#             # collection = db.Mars_hemisphere_image_urls
-#             # collection.drop()
-#             # collection.insert_many(hemisphere_image_urls)
-#             # results=collection.find()
-#             # results=[result for result in results]
-#             # results
-
-#     collection = db.Mars_facts_dict
-#     collection.drop()
-#     collection.insert_one(facts_dict)
-#     results=collection.find()
-#     results[0]
-
-            
-
-#     collection = db.Mars_weather
-#     collection.drop()
-#     collection.insert_one({"mars_weather":mars_weather})
-#     results=collection.find()
-#     results[0]
-
-#     collection = db.Mars_latest_news
-#     collection.drop()
-#     collection.insert_one(latest_news)
-#     results=collection.find()
-#     results[0]
-
-#     collection = db.Mars_featured_image_url
-#     collection.drop()
-#     collection.insert_one({"featured_image_url":featured_image_url})
-#     results=collection.find()
-#     results[0]
-#     client.close()
-
-#     return None
+    
